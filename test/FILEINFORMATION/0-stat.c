36e251c366175d10380b8282b8389c5d7ccfdb74
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-17 15:40:09 +0000

    Modified 0-stat.c to work similar to which function in shell

--- test/FILEINFORMATION/0-stat.c
+++ test/FILEINFORMATION/0-stat.c
@@ -9,31 +9,39 @@
 
 int main (int argc, char *argv[])
 {
-	char cwd[PATH_MAX];
 	struct stat dir_stat;
-	int i = 1;
+	int i = 2, j = 0, len = 0;
+	char *cmp = "_which";
 
 	/*store current working directory in current path*/
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-		return (1);
-
-	if (argc < 2)
+	if (argc < 3)
 	{
-		printf("Usage: %s filename ...\n", argv[0]);
-		return (1);
+		printf("Usage: %s _which filename ...\n", argv[0]);
+		return (-1);
 	}
-	while (argv[i])
+	while (argv[1][len])
+		len++;
+	while (argv[1][j])
 	{
-		printf("%s:", argv[i]);
-		if (stat(argv[i], &dir_stat) == 0)
-		{
-			printf("Found on directory %s\n", cwd);
-		}
+		if (argv[1][j] == cmp[j])
+			j++;
 		else
+			break;
+	}
+	if (len == j)
+	{
+		while (argv[i])
 		{
-			printf("Not found on directory %s\n", cwd);
+			if (stat(argv[i], &dir_stat) == 0)
+			{
+				printf("%s\n", argv[i]);
+			}
+			else
+			{
+				printf("Not found\n");
+			}
+			i++;
 		}
-		i++;
 	}
 	return (0);
 }
