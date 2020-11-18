e4ead578fa8b3d72245193e89c1a55253efbd627
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-18 08:47:47 -0500

    Fixing the error of function getenv

--- test/ENVIRONMENT/2-main.c
+++ test/ENVIRONMENT/2-main.c
@@ -3,12 +3,15 @@
 int main (int ac, char *av[])
 {
 	char *env;
-	(void) ac;
 
-	printf("Variable name : %s\n", av[1]);
-	env = _getenv(av[1]);
-	printf ("Using our own _getenv : %s\n", env);
-	printf("Using the real getenv : %s\n", getenv(av[1]));
-	free(env);
+	while(ac > 1)
+	{
+		printf("Variable name : %s\n", av[1]);
+		env = _getenv(av[1]);
+		printf ("Using our own _getenv : %s\n", env);
+		printf("Using the real getenv : %s\n", getenv(av[1]));
+		free(env);
+		break;
+	}
 	return (0);
 }
