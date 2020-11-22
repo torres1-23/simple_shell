de1f6dda1d6a844e49ed664adfab2444980132dc
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-22 16:33:20 -0500

    handle the non-interactive mode

--- mainshell.c
+++ mainshell.c
@@ -9,25 +9,39 @@ int main(void)
 	char *buffer = NULL, **arr = NULL, *comand = NULL;
 	size_t buffersize = 0;
 	ssize_t numc = 0;
-	int i = 0;
+	int i = 0, x = 1;
 
-	write(STDOUT_FILENO, "Alej@ Super Shell$ ", 20);
-	while ((numc = getline(&buffer, &buffersize, stdin)) != -1)
+	while(x == 1)
 	{
-		buffer[numc - 1] = '\0';
-		comand = delspace(buffer, 0);
-		built_in(comand, buffer);
-		if (numc != 1 && comand[0])
+		if (isatty(STDIN_FILENO) == 1)
 		{
-			arr = call_strtok(comand, " ");
-			execute(arr, comand, buffer);
-			i = 0;
-			while (arr[i])
-				free(arr[i++]);
-			free(arr);
+			write(STDOUT_FILENO, "Alej@ Super Shell$ ", 20);
+			x = 1;
 		}
-		write(STDOUT_FILENO, "Alej@ Super Shell$ ", 20);
-		free(comand);
+		if (isatty(STDIN_FILENO) == 0)
+		{
+			x = 0;
+		}	
+		if ((numc = getline(&buffer, &buffersize, stdin)) != -1)
+		{
+			buffer[numc - 1] = '\0';
+			comand = delspace(buffer, 0);
+			built_in(comand, buffer);
+			if (numc != 1 && comand[0])
+			{
+				arr = call_strtok(comand, " ");
+				execute(arr, comand, buffer);
+				i = 0;
+				while (arr[i])
+					free(arr[i++]);
+				free(arr);
+			}
+			free(comand);
+		}
+		else
+		{
+			break;
+		}
 	}
 	free(buffer);
 	return (0);
