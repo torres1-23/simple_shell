eaf235836e9a2829c915ba371bfcd6fa4462e807
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-24 16:37:48 +0000

    Added environ handling or executables

--- functions.c
+++ functions.c
@@ -86,17 +86,17 @@ void execute(char **args, char *c, char *b)
 	}
 	if (childn == 0)
 	{
-		if (args[0][0] == '/')
+		if ((args[0][0] == '/' && args[0][1] != '/') || (args[0][0] == '.' && args[0][1] == '/'))
 		{
-			stat = execve(args[0], args, NULL);
+			stat = execve(args[0], args, environ);
 			if (stat == 1)
 				exit(EXIT_SUCCESS);
 		}
 		path = find_path(args[0]);
-		if (path && path[0])
+		if (path && path[0] && args[0][0] != '/')
 		{
 			args[0] = _strdup(path);
-			stat = execve(args[0], args, NULL);
+			stat = execve(args[0], args, environ);
 			if (stat == 1)
 				exit(EXIT_SUCCESS);
 		}
