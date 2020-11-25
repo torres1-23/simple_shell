aa10f6433c8e054e8c10603f01b2a2d180dc949a
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-24 19:42:52 -0500

    Fixed return of built in

--- mainshell.c
+++ mainshell.c
@@ -46,8 +46,8 @@ int main(int argc __attribute__((unused)), char **argv)
 	}
 	free(buffer);
 	if ((val_isatty == 1) && (numc == 0))
-	{
 		free_bidimensional(environ);
-	}
+	if(status == 0)
+		return (status);
 	return (status1);
 }
